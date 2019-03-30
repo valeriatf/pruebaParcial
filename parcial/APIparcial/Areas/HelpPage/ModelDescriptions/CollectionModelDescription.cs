19a760e4f0bd86fdf4f9933fb7899d8f6922c640
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-03-30 11:05:22 -0400

    Se creo el Web API

---
+++ parcial/APIparcial/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace APIparcial.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ parcial/APIparcial/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIparcial.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
