f9533b929dd3fad78fb6e34e232b61456b2946e2
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-03-30 10:31:39 -0400

    commit MVC realizado

---
+++ parcial/MVCparcial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCparcial.Startup))]
+namespace MVCparcial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
