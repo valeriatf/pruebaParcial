748b8b4cc15dd182afe3768d9d42fbfe884d1db8
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-03-30 11:21:11 -0400

    commit Modelo web API

---
+++ parcial/APIparcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace APIparcial.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIparcial.Models.Product> Products { get; set; }
+    }
+}
---
+++ parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIparcial.Models
+{
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]                                                    //damos prop y tab tab seguido y nos da toda esa propiedad
+        [Display(Name = "Product Name")]
+
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        [Required]
+
+        public int Cantidad { get; set; }
+
+        [Required]
+
+        public TipoUnidad Unidad { get; set; }
+
+        [Required]
+
+        public TipoEstado Estado { get; set; }
+
+    }
+}
